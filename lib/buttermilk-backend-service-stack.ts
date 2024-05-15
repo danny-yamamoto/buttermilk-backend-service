6a48a6b690c796c0960a3c10968a2b429a97730e
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-05-15 06:17:41 +0000

    Add

--- lib/buttermilk-backend-service-stack.ts
+++ lib/buttermilk-backend-service-stack.ts
@@ -1,6 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from "aws-cdk-lib/aws-ecs";
+import * as ecsp from "aws-cdk-lib/aws-ecs-patterns";
 
 export class ButtermilkBackendServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +14,11 @@ export class ButtermilkBackendServiceStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'ButtermilkBackendServiceQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    new ecsp.ApplicationLoadBalancedFargateService(this, "MyWebServer", {
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
+      },
+      publicLoadBalancer: true,
+    });
   }
 }
